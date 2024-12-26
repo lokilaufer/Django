6b36baf633798586b32fd7e7c5c1c1ca3d953c72
Elena Lopatina <lokila667@gmail.com>
2024-12-26 17:49:47 +0400

    Views.py

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    objects = None
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(unique=True)
+
