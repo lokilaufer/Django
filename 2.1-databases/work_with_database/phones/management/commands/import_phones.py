2d94684b95dfa8022a4e40562cd14ba9403c43b7
Elena Lopatina <lokila667@gmail.com>
2024-12-26 16:46:26 +0400

    Добавление файла views.py

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,20 @@
 import csv
-
 from django.core.management.base import BaseCommand
-from phones.models import Phone
+from phone.models import Phone
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        pass
-
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
-            phones = list(csv.DictReader(file, delimiter=';'))
-
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+        with open('phones.csv') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=';')
+            for row in reader:
+                phone = Phone(
+                    id=row['id'],
+                    name=row['name'],
+                    price=row['price'],
+                    image=row['image'],
+                    release_date=row['release_date'],
+                    lte_exists=(row['lte_exists'] == 'True'),
+                    slug=slugify(row['name'])
+                )
+                phone.save()
