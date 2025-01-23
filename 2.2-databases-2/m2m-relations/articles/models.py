c64f3c66538e3261555a5ef6f145f142f5aaad33
Elena Lopatina <lokila667@gmail.com>
2025-01-23 22:55:59 +0400

    New

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,36 +1,26 @@
 from django.db import models
 
-class Article(models.Model):
-    title = models.CharField(max_length=256, verbose_name='Название')
-    text = models.TextField(verbose_name='Текст')
-    published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Имя')
+    subject = models.CharField(max_length=10, verbose_name='Предмет')
 
     class Meta:
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
-        ordering = ['-published_at']
-
-    def __str__(self):
-        return self.title
-
-class Tag(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Тег')
-    articles = models.ManyToManyField(Article, related_name='tags', through='Scope')
-
-    class Meta:
-        verbose_name = 'Тег'
-        verbose_name_plural = 'Теги'
+        verbose_name = 'Учитель'
+        verbose_name_plural = 'Учителя'
 
     def __str__(self):
         return self.name
 
 
-class Scope(models.Model):
-    article = models.ForeignKey(Article, related_name='scopes', on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, related_name='scopes', on_delete=models.CASCADE)
-    is_main = models.BooleanField(verbose_name='Основной')
+class Student(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Имя')
+    teachers = models.ManyToManyField(Teacher, related_name='students', verbose_name='Учителя')
+    group = models.CharField(max_length=10, verbose_name='Класс')
 
     class Meta:
-        ordering = ['-is_main']
+        verbose_name = 'Ученик'
+        verbose_name_plural = 'Ученики'
 
+    def __str__(self):
+        return self.name
